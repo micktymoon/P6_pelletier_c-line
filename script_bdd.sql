b59382a2ab4012dbb3ba96901fea7fbb7474193a
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-09-23 17:54:26 +0200

    changements demandés par pablo

--- script_bdd.sql
+++ script_bdd.sql
@@ -2,11 +2,33 @@ CREATE DATABASE ocPizza;
 USE ocPizza;
 CREATE  TABLE Client (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    civilite ENUM('M', 'Mme') NOT NULL,
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
     e_mail VARCHAR(320) NOT NULL,
     numero_telephone VARCHAR(20) NOT NULL,
     mot_de_passe VARCHAR(40) CHARACTER SET ascii NOT NULL,
+    PRIMARY KEY (id)
+    )
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE  TABLE Employe (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    civilite ENUM('M', 'Mme') NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    numero_telephone VARCHAR(20) NOT NULL,
+    grade ENUM('manager', 'cuisinier', 'serveur', 'livreur') NOT NULL,
+    PRIMARY KEY (id)
+    )
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE Adresse (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    voie VARCHAR(50) NOT NULL,
+    complement_adresse VARCHAR(50),
+    code_postal VARCHAR(10) NOT NULL,
+    ville VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
     )
     ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -24,8 +46,7 @@ CREATE TABLE Restaurant (
 
 CREATE TABLE Paiement (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    prix_total DECIMAL(5, 2) NOT NULL,
-    type INT NOT NULL,
+    type ENUM('sur_place', 'a_la_livraison', 'en_ligne') NOT NULL,
     date_paiement DATETIME NOT NULL,
     PRIMARY KEY (id)
     )
@@ -42,38 +63,57 @@ CREATE TABLE Pizza (
 CREATE TABLE Ingredient (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nom VARCHAR(50) NOT NULL,
+    unite ENUM('centilitre', 'gramme', 'unite') NOT NULL,
     PRIMARY KEY (id)
     )
     ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE Commande (
+CREATE TABLE CommandeWeb (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     date_commande DATETIME NOT NULL,
-    statut INT NOT NULL,
-    adresse_destinataire VARCHAR(50) NOT NULL,
-    adresse_complement_destinataire VARCHAR(50),
-    adresse_voie VARCHAR(50) NOT NULL,
-    adresse_complement VARCHAR(50),
-    adresse_code_postal VARCHAR(10) NOT NULL,
-    adresse_ville VARCHAR(50) NOT NULL,
+    statut ENUM('en_attente', 'en_preparation', 'preparee', 'en_livraison', 'livree', 'servi') NOT NULL,
+    prix_total DECIMAL(5, 2) NOT NULL,
     PRIMARY KEY (id)
     )
     ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE LigneCommande (
+CREATE TABLE CommandeSurPlace (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    date_commande DATETIME NOT NULL,
+    statut ENUM('en_attente', 'en_preparation', 'preparee', 'en_livraison', 'livree', 'servi') NOT NULL,
+    prix_total DECIMAL(5, 2) NOT NULL,
+    PRIMARY KEY (id)
+    )
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE DetailCommandeWeb (
     pizza_id INT UNSIGNED NOT NULL,
-    commande_id INT UNSIGNED NOT NULL,
-    quantité INT NOT NULL,
+    commande_web_id INT UNSIGNED NOT NULL,
+    quantite INT NOT NULL,
+    taille ENUM('S', 'M', 'L') NOT NULL,
     prix DECIMAL(5,2) NOT NULL,
-    PRIMARY KEY (pizza_id, commande_id),
+    PRIMARY KEY (pizza_id, commande_web_id),
     FOREIGN KEY (pizza_id) REFERENCES Pizza(id),
-    FOREIGN KEY (commande_id) REFERENCES Commande(id)
+    FOREIGN KEY (commande_web_id) REFERENCES CommandeWeb(id)
+    )
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE DetailCommandeSurPlace (
+    pizza_id INT UNSIGNED NOT NULL,
+    commande_sur_place_id INT UNSIGNED NOT NULL,
+    quantite INT NOT NULL,
+    taille ENUM('S', 'M', 'L') NOT NULL,
+    prix DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (pizza_id, commande_sur_place_id),
+    FOREIGN KEY (pizza_id) REFERENCES Pizza(id),
+    FOREIGN KEY (commande_sur_place_id) REFERENCES CommandeSurPlace(id)
     )
     ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IngredientPizza (
     pizza_id INT UNSIGNED NOT NULL,
     ingredient_id INT UNSIGNED NOT NULL,
+    quantite INT NOT NULL,
     PRIMARY KEY (pizza_id, ingredient_id),
     FOREIGN KEY (pizza_id) REFERENCES Pizza(id),
     FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id)
@@ -83,7 +123,8 @@ CREATE TABLE IngredientPizza (
 CREATE TABLE Stock (
     restaurant_id INT UNSIGNED NOT NULL,
     ingredient_id INT UNSIGNED NOT NULL,
-    quantité INT NOT NULL,
+    quantite INT NOT NULL,
+    unite ENUM('boite', 'paquet', 'bouteille', 'sachet') NOT NULL,
     PRIMARY KEY (restaurant_id, ingredient_id),
     FOREIGN KEY (restaurant_id) REFERENCES Restaurant(id),
     FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id)
--- script_fill_db.sql
+++ script_fill_db.sql
@@ -1,8 +1,8 @@
-INSERT INTO Ingredient (nom)
-VALUES ('sauce tomate'), ('mozzarella'), ('olives noires'), ('roquette'),
-('anchois'), ('câpres'), ('jambon'), ('champignons'), ('artichauds marinés'),
-('chorizo'), ('merguez'), ('oignons rouges'), ('piments vinaigrés'),
-('viandes hachées'), ('boursin'), ('chèvre'), ('bleu'), ('raclette'), ('emmental');
+INSERT INTO Ingredient (nom, unite)
+VALUES ('sauce tomate', 'centilitre'), ('mozzarella', 'gramme'), ('olives noires', 'unite'), ('roquette', 'gramme'),
+('anchois', 'gramme'), ('câpres', 'gramme'), ('jambon', 'unite'), ('champignons', 'gramme'), ('artichauds marinés', 'gramme'),
+('chorizo', 'unite'), ('merguez', 'unite'), ('oignons rouges', 'unite'), ('piments vinaigrés', 'gramme'),
+('viandes hachées', 'gramme'), ('boursin', 'gramme'), ('chèvre', 'gramme'), ('bleu', 'gramme'), ('raclette', 'gramme'), ('emmental', 'gramme');
 
 INSERT INTO Pizza (nom, prix)
 VALUES ('margharita', 11.90), ('napolitaine', 12.90),
@@ -10,39 +10,51 @@ VALUES ('margharita', 11.90), ('napolitaine', 12.90),
 ('boursin', 13.90), ('regina', 13.90), ('5 fromages', 13.90);
 
 
-INSERT INTO Client (nom, prenom, e_mail, numero_telephone, mot_de_passe)
-VALUES ('Banane', 'Jonas', 'j.banane@hotmail.fr', '0601020304', 'banane93'),
-('Zuki', 'Mika', 'm.zuki@hotmail.fr', '0612345678', 'pgmdu77.'),
-('Lee', 'Mira', 'm.lee@hotmail.fr', '0611223344', 'blabla28');
+INSERT INTO Client (civilite, nom, prenom, e_mail, numero_telephone, mot_de_passe)
+VALUES ('M', 'Banane', 'Jonas', 'j.banane@hotmail.fr', '0601020304', 'banane93'),
+('M', 'Zuki', 'Mika', 'm.zuki@hotmail.fr', '0612345678', 'pgmdu77.'),
+('M', 'Lee', 'Mira', 'm.lee@hotmail.fr', '0611223344', 'blabla28');
 
 INSERT INTO Restaurant (nom, adresse_voie, adresse_code_postal, adresse_ville)
 VALUES ('OC pizza Nord', '12 rue du Général Leclerc', '75008', 'Paris'),
 ('OC pizza Sud', '5 rue Geronimo', '75014', 'Paris'),
 ('OC pizza Est', '8 rue des jardins', '75020', 'Paris'),
-('OC pizza Ouest', '20 rue de la Reine', '75006', 'Paris')
+('OC pizza Ouest', '20 rue de la Reine', '75006', 'Paris');
 
-INSERT INTO IngredientPizza (pizza_id, ingredient_id)
-VALUES (1, 1), (1, 2), (1, 3), (1, 4),
-(2, 1), (2, 2), (2, 3), (2, 5), (2, 6),
-(3, 1), (3, 2), (3, 7), (3, 8), (3, 9), (3, 3),
-(4, 1), (4, 2), (4, 10), (4, 11), (4, 12), (4, 13),
-(5, 1), (5, 2), (5, 14), (5, 8), (5, 12), (5, 15),
-(6, 1), (6, 2), (6, 7), (6, 8), (7, 1), (7, 2), (7, 16), (7, 17), (7, 18), (7, 19);
+INSERT INTO Adresse (voie, code_postal, ville)
+VALUES ('20 rue de la maison verte', 75008, 'Paris'),
+('5 rue de la Vallée', 75014, "Paris"), ('9 avenue des Poirreaux', 75020, 'Paris');
 
-INSERT INTO Stock (restaurant_id, ingredient_id, quantité)
-VALUES (1, 1, 100), (1, 2, 100), (1, 3, 80), (1, 4, 50),
-(1, 5, 50), (1, 6, 40), (1, 7, 100), (1, 8, 100), (1, 9, 50),
-(1, 10, 80), (1, 11, 80), (1, 12, 80), (1, 13, 70), (1, 14, 60),
-(1, 15, 40), (1, 16, 90), (1, 17, 90), (1, 18, 90), (1, 19, 90),
-(2, 1, 130), (2, 2, 130), (2, 3, 100), (2, 4, 80),
-(2, 5, 100), (2, 6, 80), (2, 7, 150), (2, 8, 150), (2, 9, 70),
-(2, 10, 100), (2, 11, 100), (2, 12, 100), (2, 13, 70), (2, 14, 60),
-(2, 15, 40), (2, 16, 150), (2, 17, 150), (2, 18, 150), (2, 19, 150),
-(3, 1, 200), (3, 2, 200), (3, 3, 200), (3, 4, 80),
-(3, 5, 70), (3, 6, 50), (3, 7, 250), (3, 8, 250), (3, 9, 70),
-(3, 10, 100), (3, 11, 100), (3, 12, 100), (3, 13, 70), (3, 14, 60),
-(3, 15, 40), (3, 16, 300), (3, 17, 300), (3, 18, 300), (3, 19, 300),
-(4, 1, 200), (4, 2, 200), (4, 3, 200), (4, 4, 80),
-(4, 5, 70), (4, 6, 50), (4, 7, 250), (4, 8, 250), (4, 9, 70),
-(4, 10, 100), (4, 11, 100), (4, 12, 100), (4, 13, 70), (4, 14, 60),
-(4, 15, 40), (4, 16, 300), (4, 17, 300), (4, 18, 300), (4, 19, 300);
+INSERT INTO Employe (civilite, nom, prenom, numero_telephone, grade)
+VALUES ('Mme', 'Jardin', 'Anais', '0627954368', 'serveur'),
+('M', 'Tartanpion', 'Max', '0654793158', 'Manager'),
+('M', 'Patate', 'Jack', '0647958631', 'Cuisinier'),
+('Mme', 'Velo', 'Ema', '0612358496', 'Livreur');
+
+
+INSERT INTO IngredientPizza (pizza_id, ingredient_id, quantite)
+VALUES (1, 1, 20), (1, 2, 100), (1, 3, 5), (1, 4, 20),
+(2, 1, 20), (2, 2, 100), (2, 3, 5), (2, 5, 100), (2, 6, 50),
+(3, 1, 20), (3, 2, 100), (3, 7, 2), (3, 8, 50), (3, 9, 20), (3, 3, 5),
+(4, 1, 20), (4, 2, 100), (4, 10, 6), (4, 11, 2), (4, 12, 1), (4, 13, 10),
+(5, 1, 20), (5, 2, 100), (5, 14, 100), (5, 8, 50), (5, 12, 1), (5, 15, 20),
+(6, 1, 20), (6, 2, 100), (6, 7, 2), (6, 8, 50),
+(7, 1, 20), (7, 2, 100), (7, 16, 20), (7, 17, 20), (7, 18, 20), (7, 19, 20);
+
+INSERT INTO Stock (restaurant_id, ingredient_id, quantite, unite)
+VALUES (1, 1, 100, 'bouteille'), (1, 2, 100, 'paquet'), (1, 3, 80, 'boite'), (1, 4, 50, 'sachet'),
+(1, 5, 50, 'boite'), (1, 6, 40, 'boite'), (1, 7, 100, 'paquet'), (1, 8, 100, 'boite'), (1, 9, 50, 'boite'),
+(1, 10, 80, 'paquet'), (1, 11, 80, 'paquet'), (1, 12, 80, 'sachet'), (1, 13, 70, 'boite'), (1, 14, 60, 'paquet'),
+(1, 15, 40, 'paquet'), (1, 16, 90, 'paquet'), (1, 17, 90, 'paquet'), (1, 18, 90, 'paquet'), (1, 19, 90, 'paquet'),
+(2, 1, 130, 'bouteille'), (2, 2, 130, 'paquet'), (2, 3, 100, 'boite'), (2, 4, 80, 'sachet'),
+(2, 5, 100, 'boite'), (2, 6, 80, 'boite'), (2, 7, 150, 'paquet'), (2, 8, 150, 'boite'), (2, 9, 70, 'boite'),
+(2, 10, 100, 'paquet'), (2, 11, 100, 'paquet'), (2, 12, 100, 'sachet'), (2, 13, 70, 'boite'), (2, 14, 60, 'paquet'),
+(2, 15, 40, 'paquet'), (2, 16, 150, 'paquet'), (2, 17, 150, 'paquet'), (2, 18, 150, 'paquet'), (2, 19, 150, 'paquet'),
+(3, 1, 200, 'bouteille'), (3, 2, 200, 'paquet'), (3, 3, 200, 'boite'), (3, 4, 80, 'sachet'),
+(3, 5, 70, 'boite'), (3, 6, 50, 'boite'), (3, 7, 250, 'paquet'), (3, 8, 250, 'boite'), (3, 9, 70, 'boite'),
+(3, 10, 100, 'paquet'), (3, 11, 100, 'paquet'), (3, 12, 100, 'sachet'), (3, 13, 70, 'boite'), (3, 14, 60, 'paquet'),
+(3, 15, 40, 'paquet'), (3, 16, 300, 'paquet'), (3, 17, 300, 'paquet'), (3, 18, 300, 'paquet'), (3, 19, 300, 'paquet'),
+(4, 1, 200, 'bouteille'), (4, 2, 200, 'paquet'), (4, 3, 200, 'boite'), (4, 4, 80, 'sachet'),
+(4, 5, 70, 'boite'), (4, 6, 50, 'boite'), (4, 7, 250, 'paquet'), (4, 8, 250, 'boite'), (4, 9, 70, 'boite'),
+(4, 10, 100, 'paquet'), (4, 11, 100, 'paquet'), (4, 12, 100, 'sachet'), (4, 13, 70, 'boite'), (4, 14, 60, 'paquet'),
+(4, 15, 40, 'paquet'), (4, 16, 300, 'paquet'), (4, 17, 300, 'paquet'), (4, 18, 300, 'paquet'), (4, 19, 300, 'paquet');
